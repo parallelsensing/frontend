43f3e46a902e4de1727d1648c368d79b35237d5e
tzq1032 <13463239900@163.com>
2024-03-20 15:13:56 +0800

    上线测试

--- src/pages/home-entre.vue
+++ src/pages/home-entre.vue
@@ -7,6 +7,7 @@
       <img width="100" src="@/assets/images/logo.jpeg" />
       <img width="100" src="@/assets/svgs/vue.svg" />
     </h2>
+    <h2>田泽钦测试成功</h2>
     <p><router-link to="/work">work</router-link></p>
   </main>
 </template>
