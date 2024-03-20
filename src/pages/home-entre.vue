22ac57531cb7818e07972caefacf31eaa0154d56
tzq1032 <13463239900@163.com>
2024-03-20 15:20:45 +0800

    测试

--- src/pages/home-entre.vue
+++ src/pages/home-entre.vue
@@ -8,6 +8,7 @@
       <img width="100" src="@/assets/svgs/vue.svg" />
     </h2>
     <h2>田泽钦测试成功</h2>
+    <h2>test</h2>
     <p><router-link to="/work">work</router-link></p>
   </main>
 </template>
