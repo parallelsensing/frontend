1993ce0c8d453ec3f83c1e5309fd9fa5d88c49f6
tzq1032 <13463239900@163.com>
2024-03-26 09:12:47 +0800

    reback

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,55 +1,33 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeEntre from '@/pages/home-entre.vue';
+import NotFound from '@/pages/error-404.vue';
 
-import NotFound from "../pages/404.vue"
-import Platform from "../pages/Platform.vue";
-import Home from "../components/Home.vue";
-import Login from "../pages/Login.vue";
-import Map from "../pages/Map.vue";
-
-const routes: Array <RouteRecordRaw> = [
-  {
-    name: 'login',
-    path: '/',
-    component: Login,
-    props: true,
-  },
-  {
-    name: 'Map',
-    path: '/map',
-    component: Map,
-    props: true,
-  },
-  {
-    name: 'home',
-    path: '/home',
-    component: Home,
-    props: true,
-  },
-
-  {
-    name: 'platform',
-    path: '/platform',
-    component: Platform,
-    props: true,
-    meta: { title: 'model' }
-  },
-  {
-    name: '404',
-    path: '/404',
-    component: NotFound
-  },
-];
-
-let history = createWebHistory();
 const router = createRouter({
-  history,
-  routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeEntre
+    },
+    {
+      path: '/work',
+      name: 'work',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('@/pages/main-surface.vue')
+    },
+    {
+      name: '404',
+      path: '/404',
+      component: NotFound
+    },
+    {
+      path: '/:catchAll(.*)', // 此处需特别注意至于最底部
+      redirect: '/404'
+    }
+  ]
 });
-// router.beforeEach((to, _from, next) => {
-//   if (to.meta.title) {
-//     document.title = `IEEE ITSC 2022 | ${to.meta.title}`;
-//   }
-//   next();
-// });
 
 export default router;
