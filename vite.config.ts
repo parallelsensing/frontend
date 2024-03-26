64c30fd4721ced85a5c797bc70fe48dbb0083d88
tzq1032 <13463239900@163.com>
2024-03-26 11:16:12 +0800

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -37,3 +37,21 @@ export default defineConfig(({command})=>{
   }
 
 })
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// // import { createVuePlugin } from 'vite-plugin-vue';
+// //mock插件提供方法
+// import { viteMockServe } from 'vite-plugin-mock'
+
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({command})=>{
+//  return{
+//   plugins: [vue(),
+//     viteMockServe({
+//       enable: command==='serve',
+//     }),
+//   ]
+//  }
+// })
