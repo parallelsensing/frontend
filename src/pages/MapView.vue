85dcd67450ccb108341cfdf9e49da15659a542f3
tzq1032 <13463239900@163.com>
2024-03-27 17:29:30 +0800

    add map

--- src/pages/MapView.vue
+++ src/pages/MapView.vue
@@ -1,41 +1,83 @@
 <template>
-      <div ref="mapContainer" class="map-container" id="map"></div>
+  <MapboxMap style="height: 100vh"
+    access-token="pk.eyJ1IjoidHpxMTAzMiIsImEiOiJjbHUyZWxuYTUwMWlrMndsM3VxaHJpcmd6In0.qE_R5khyiy6PYCjUuTkywQ"
+    :center="mapCenter" :zoom="5" :pitch="62" :bearing="-20" @mb-created="onMapCreated">
+    <!-- <MapboxMarker :lng-lat="mapCenter" @click="TOPlatform" /> -->
+  </MapboxMap>
+  <div ref="markerInfo" id="markerInfo">
+    <p>点击进入中科院自动化研究所</p>
+    <img src="/img/CASIA.jpg" alt="图片">
+  </div>
 </template>
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
+import { MapboxMap } from '@studiometa/vue-mapbox-gl';
 import mapboxgl from 'mapbox-gl';
 
+
+
+import { ref, } from 'vue'
+import { useRouter } from 'vue-router';
 let router = useRouter();
-const mapContainer = ref();
-const bbox = [[116.33, 39.97], [116.34, 39.98]];
-onMounted(() => {
-  mapContainer.value = document.getElementById('map');
-    const map = new mapboxgl.Map({
-      container: 'map',
-      center: [116.332403, 39.979241],
-      zoom: 15.1, // starting zoom
+
+const mapCenter = ref<number[]>([116.3262, 39.978041]);
+const map = ref();
+const markerInfo = ref()
+const marker = new mapboxgl.Marker()
+const onMapCreated = (mapInstance:any) => {
+  // Store the map instance for later use if needed
+  map.value = mapInstance;
+  console.log(map.value);
+  
+  map.value.on("load", function () {
+    map.value.flyTo({
+      center: [116.3262, 39.978041],
+      zoom: 17.1, // starting zoom
       pitch: 62, // starting pitch
       bearing: -20, // starting bearing
-      accessToken: "pk.eyJ1IjoidHpxMTAzMiIsImEiOiJjbHUyZWxuYTUwMWlrMndsM3VxaHJpcmd6In0.qE_R5khyiy6PYCjUuTkywQ"
+      speed: 1.5, // 动画速度
+      curve: 1, // 动画曲线
+      // easing:
     });
-    console.log(map);
-    
-    map.on("click", function (e:any) { // Example bounding box
-      if (e.lngLat.lng >= bbox[0][0] && 
-          e.lngLat.lng <= bbox[1][0] && 
-          e.lngLat.lat >= bbox[0][1] && 
-          e.lngLat.lat <= bbox[1][1]) {
-            router.push('/platform')
-      } 
-    });
-});
+  });
+
+  marker.setLngLat([116.3262, 39.978041]) // Set marker position
+    .addTo(mapInstance);
+  marker.getElement().addEventListener("click", function () {
+    router.push('/platform')
+  });
+  marker.getElement().addEventListener("mouseover", function (e) {
+    markerInfo.value.style.display = "block";
+    markerInfo.value.style.left = e.pageX + 10 + "px"; // 位置调整
+    markerInfo.value.style.top = e.pageY + 10 + "px"; // 位置调整
+    map.value.getCanvas().style.cursor = 'pointer';
+  });
+  marker.getElement().addEventListener("mouseout", function () {
+    markerInfo.value.style.display = "none";
+    map.value.getCanvas().style.cursor = '';
+  });
+
+};
+
+
+
+
 </script>
-<style>
-.map-container {
-      position: absolute;
-      top: 0;
-      bottom: 0;
-      width: 100%;
+
+<style scoped>
+#markerInfo {
+  position: absolute;
+  background-color: #fff;
+  padding: 10px;
+  border: 1px solid #ccc;
+  display: none;
+  width: auto;
+  height: 200px;
+
 }
+#markerInfo img {
+    display: block;
+    margin: 0 auto;
+    width: 200px; /* 设置图片宽度为父div的100% */
+    height: auto;
+  }
 </style>
