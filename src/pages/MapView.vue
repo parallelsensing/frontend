80ef6160575e2c0df8f83ac85e9e1dee8ff0d83a
tzq1032 <13463239900@163.com>
2024-03-29 17:27:43 +0800

    encapsulation mapScene

--- src/pages/MapView.vue
+++ src/pages/MapView.vue
@@ -1,8 +1,7 @@
 <template>
   <MapboxMap style="height: 100vh"
     access-token="pk.eyJ1IjoidHpxMTAzMiIsImEiOiJjbHUyZWxuYTUwMWlrMndsM3VxaHJpcmd6In0.qE_R5khyiy6PYCjUuTkywQ"
-    :center="mapCenter" :zoom="5" :pitch="62" :bearing="-20" @mb-created="onMapCreated">
-    <!-- <MapboxMarker :lng-lat="mapCenter" @click="TOPlatform" /> -->
+    :center="mapCenter" @mb-created="onMapCreated">
   </MapboxMap>
   <div ref="markerInfo" id="markerInfo">
     <p>点击进入中科院自动化研究所</p>
@@ -11,58 +10,24 @@
 </template>
 <script setup lang="ts">
 import { MapboxMap } from '@studiometa/vue-mapbox-gl';
-import mapboxgl from 'mapbox-gl';
-
-
 
 import { ref, } from 'vue'
-import { useRouter } from 'vue-router';
-let router = useRouter();
+// import { useRouter } from 'vue-router';
+import MapScene from '@/units/map/index'
+// let router = useRouter();
 
 const mapCenter = ref<number[]>([116.3262, 39.978041]);
 const map = ref();
-const markerInfo = ref()
-const marker = new mapboxgl.Marker()
-const onMapCreated = (mapInstance:any) => {
+const markerInfo = ref<HTMLElement>()
+
+const onMapCreated = (mapInstance: any) => {
   // Store the map instance for later use if needed
   map.value = mapInstance;
-  console.log(map.value);
-  
-  map.value.on("load", function () {
-    map.value.flyTo({
-      center: [116.3262, 39.978041],
-      zoom: 17.1, // starting zoom
-      pitch: 62, // starting pitch
-      bearing: -20, // starting bearing
-      speed: 1.5, // 动画速度
-      curve: 1, // 动画曲线
-      // easing:
-    });
-  });
-
-  marker.setLngLat([116.3262, 39.978041]) // Set marker position
-    .addTo(mapInstance);
-  marker.getElement().addEventListener("click", function () {
-    router.push('/platform')
-  });
-  marker.getElement().addEventListener("mouseover", function (e) {
-    markerInfo.value.style.display = "block";
-    markerInfo.value.style.left = e.pageX + 10 + "px"; // 位置调整
-    markerInfo.value.style.top = e.pageY + 10 + "px"; // 位置调整
-    map.value.getCanvas().style.cursor = 'pointer';
-  });
-  marker.getElement().addEventListener("mouseout", function () {
-    markerInfo.value.style.display = "none";
-    map.value.getCanvas().style.cursor = '';
-    console.log(111);
-    
-  });
-
+  let mapScene = new MapScene(map.value)
+  mapScene.flyTo()
+  mapScene.addMarkerInfo(markerInfo.value)
+  mapScene.toPlatformPage()
 };
-
-
-
-
 </script>
 
 <style scoped>
@@ -76,10 +41,12 @@ const onMapCreated = (mapInstance:any) => {
   height: 200px;
 
 }
+
 #markerInfo img {
-    display: block;
-    margin: 0 auto;
-    width: 200px; /* 设置图片宽度为父div的100% */
-    height: auto;
-  }
+  display: block;
+  margin: 0 auto;
+  width: 200px;
+  /* 设置图片宽度为父div的100% */
+  height: auto;
+}
 </style>
