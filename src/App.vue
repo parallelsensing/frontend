7f70e125deac24fa95a847386f2caae752ad95dc
tzq1032 <13463239900@163.com>
2024-04-03 17:52:58 +0800

    add markers

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,22 @@
 
 <script setup lang="ts" >
 // 测试request
+// import axios from 'axios'
 
 import  {onMounted} from 'vue';
 import { reqLogin } from './api/user';
 onMounted(()=>{
  reqLogin({username:'111111',password:'123456'})
+//  axios.get('/api')
+//     .then(function (response) {
+//       // 处理成功响应
+//     console.log(response);
+    
+//     })
+//     .catch(function (error) {
+//       // 处理错误
+//       console.error('Error fetching user information:', error);
+//     });
+
 })
 </script>
