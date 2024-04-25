97c4dfb2c90a758f282f31a32b699b404fc73c1b
tzq1032 <13463239900@163.com>
2024-04-25 15:18:56 +0800

    登录初始化

--- src/pages/Login-view.vue
+++ src/pages/Login-view.vue
@@ -29,7 +29,7 @@ import { useUserStore } from '@/stores/users';
 import { useRouter } from 'vue-router';
 import { getTime } from '@/utils/time'
 import { successAlert, errorAlert } from '@/utils/alert'
-let loginForm = reactive({ phone: '123456', password: '123456' })
+let loginForm = reactive({ phone: '15801585812', password: '123456' })
 let useStore = useUserStore()
 let router = useRouter();
 let login = async () => {
